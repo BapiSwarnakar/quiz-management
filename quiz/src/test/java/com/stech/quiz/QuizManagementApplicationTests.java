24760e8c4587b14bfb726d0f233ebe4b525f0771
Mejora112 <bapi@mejorainfotech.in>
2025-11-01 22:30:39 +0530

    first commit

---
+++ quiz/src/test/java/com/stech/quiz/QuizManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stech.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
